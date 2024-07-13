2aec0c7721a21102768ddea0b86af2f3c86a62fc
Gayanuka Bulegoda <grbulegoda@gmail.com>
2024-07-13 16:47:13 +0530

    Jakartha EE Introduction | Servlet Specification | Tomcat

---
+++ src/main/java/lk/ijse/aad/gdse68/jakarthaee_intro/Demo.java
@@ -0,0 +1,32 @@
+package lk.ijse.aad.gdse68.jakarthaee_intro;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static java.lang.Thread.currentThread;
+
+@WebServlet(urlPatterns = "/demo", loadOnStartup = 2)
+public class Demo extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Hello Init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello Get Method");
+        System.out.println(currentThread().getName()); //different threads for different requests
+        System.out.println("Hello Get Method");
+    }
+
+    @Override
+    public void destroy() { //
+        System.out.println("Destroyed..........");
+    }
+}
