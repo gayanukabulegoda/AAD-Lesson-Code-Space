a5be13db6aa712ac011361511188001cc901f373
gayanukabulegoda <grbulegoda@gmail.com>
2024-08-02 00:52:40 +0530

    Spring | Programming paradigms | POJO | Spring Bean | Application Context Types

--- Back-end/src/main/java/lk/ijse/aad/gdse68/studentmanagement/util/Util.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.aad.gdse68.studentmanagement.util;
-
-import java.util.UUID;
-
-public class Util {
-    public static String generateId() {
-        return UUID.randomUUID().toString();
-    }
-}
