d6c6b31af13c4c367a469f735964eab1828a487b
gayanukabulegoda <grbulegoda@gmail.com>
2024-08-04 23:55:40 +0530

    dependency injection types

--- src/main/java/lk/ijse/Spring_Intro/user/User.java
+++ src/main/java/lk/ijse/Spring_Intro/user/User.java
@@ -14,13 +14,8 @@ public class User {
     @Wow
     private GoodGirl goodGirl;
 
-//    public User(GoodGirl goodGirl) {
-//        System.out.println(goodGirl);
-//    }
-
     @PostConstruct
     public void init() {
-        // System.out.println(goodGirl);
         goodGirl.love();
     }
 }
