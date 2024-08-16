29188f776dc161df54610034715f78c11f5eb33f
gayanukabulegoda <grbulegoda@gmail.com>
2024-08-16 11:57:25 +0530

    spring web MVC intro | mapping types

---
+++ src/main/java/lk/ijse/introwithspringweb/config/WebAppConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.introwithspringweb.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.introwithspringweb")
+public class WebAppConfig {
+}
