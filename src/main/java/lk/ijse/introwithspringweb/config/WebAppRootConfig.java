9773e3fb673482bad5a849d09ce13558e73af6cd
gayanukabulegoda <grbulegoda@gmail.com>
2024-08-19 15:25:32 +0530

    mapping types | ResponseEntity | headers | map vs multi-value map

--- src/main/java/lk/ijse/introwithspringweb/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/introwithspringweb/config/WebAppRootConfig.java
@@ -2,8 +2,10 @@ package lk.ijse.introwithspringweb.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
+@EnableWebMvc
 public class WebAppRootConfig {
 }
--- src/main/java/lk/ijse/introwithspringweb/controller/DemoController.java
+++ src/main/java/lk/ijse/introwithspringweb/controller/DemoController.java
@@ -1,9 +1,13 @@
 package lk.ijse.introwithspringweb.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.introwithspringweb.dto.Customer;
+import lk.ijse.introwithspringweb.dto.Item;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/demo")
@@ -61,4 +65,63 @@ public class DemoController {
     public String paramsTest() {
         return "All are tested";
     }
-}
+
+    // query parameters with multiple values: This matches requests with multiple query parameters.
+    @PostMapping(params = {"name", "quantity"})
+    public String paramData(@RequestParam("name") String param1, @RequestParam("quantity") String param2) {
+        return "Param data is : "+ param1 + " and " + param2;
+    }
+
+    // path variable with regex and custom header: This matches a path segment with a variable constrained by a regular expression and a custom header.
+    @GetMapping(value = "/path-segment/{id:C\\d{3}}", headers = "X-number")
+    public String pathVariableWithRegexAndHeader(@PathVariable("id") String id, @RequestHeader("X-number") int num) {
+        return "Path variable including regex works accurately with : " + id + " and Header is : " + num;
+    }
+
+    // consumes: This specifies the media type that the endpoint can consume, allowing the server to process requests with the specified media type.
+    // @PostMapping(consumes = "application/json")
+    // ----- OR -----
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String saveJSON() {
+        return "JSON data saved successfully";
+    }
+
+    // dynamic path variable with conditional response: This matches a path segment with a dynamic variable and returns a response based on the value of the variable.
+    @PostMapping("/dynamic/{value:\\d{2}}")
+    public ResponseEntity<String> returnDynamicData(@PathVariable("value") int incomingValue) {
+       if (incomingValue % 2 == 0) {
+           return ResponseEntity.ok("Even Number");
+       }
+       return ResponseEntity.ok("Odd Number");
+    }
+
+    // handle dynamic query parameters with Map: This matches requests with dynamic query parameters and stores them in a map.
+    @PostMapping(value = "/mapParams", params = {"id", "desc"})
+    public String handleMaps(@RequestParam("id") String id, @RequestParam("desc") String desc, @RequestParam Map<String, String> params) {
+        System.out.println(params);
+        return "Handle Map with Params : " + params;
+    }
+
+    // handle dynamic query parameters with MultiValueMap: This matches requests with dynamic query parameters and stores them in a MultiValueMap.
+    @PostMapping(value = "/multiMapParams", params = {"id", "desc"})
+    public String handleMultiMaps(@RequestParam("id") String id, @RequestParam("desc") String desc, @RequestParam MultiValueMap<String, String> params) {
+        System.out.println(params);
+        return "Handle Map with Params : " + params;
+    }
+
+    // handle dynamic query parameters with MultiValueMap and set to DTO: This matches requests with dynamic query parameters, stores them in a MultiValueMap, and sets them to a DTO.
+    @PostMapping(value = "/multiMapParamsToDto", params = {"id","desc"})
+    public String handleMultiMapsWithDTO(@RequestParam ("id") String id, @RequestParam ("desc")String desc, @RequestParam MultiValueMap<String,String> params, Item item){
+        System.out.println(item);
+        return "Handle Maps with params and set to DTO "+ params;
+    }
+
+    // convert JSON to DTO : This converts JSON data in the request body to a DTO.
+    @PostMapping(value = "/customer", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String JSONToTO(@RequestBody Customer customer) {
+        System.out.println(customer.getId());
+        System.out.println(customer.getName());
+        System.out.println(customer.getEmail());
+        return "Customer : " + customer.getName() + " retrieved successfully";
+    }
+ }
