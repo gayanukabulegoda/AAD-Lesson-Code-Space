64c22018aa72a565eea430e393b6ad7bd6b48488
gayanukabulegoda <grbulegoda@gmail.com>
2024-08-31 00:43:40 +0530

    hibernate | JPA and JPL | service layer implementation

--- src/main/java/lk/ijse/notetaker/controller/NoteController.java
+++ src/main/java/lk/ijse/notetaker/controller/NoteController.java
@@ -1,9 +1,10 @@
 package lk.ijse.notetaker.controller;
 
-import lk.ijse.notetaker.bo.NoteBo;
+import lk.ijse.notetaker.service.NoteService;
 import lk.ijse.notetaker.dto.NoteDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,32 +16,34 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NoteController {
     @Autowired
-    private final NoteBo noteBo;
+    private final NoteService noteService;
 
     //ToDo: CRUD of the Note
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> createNote(@RequestBody NoteDTO noteDTO) {
-        var isSaved = noteBo.saveNote(noteDTO);
+        var isSaved = noteService.saveNote(noteDTO);
         return ResponseEntity.ok(isSaved);
     }
 
     @GetMapping(value ="allNotes", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<NoteDTO> getAllNotes() {
-        return noteBo.getAllNotes();
+        return noteService.getAllNotes();
     }
 
     @GetMapping(value = "/{noteId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public NoteDTO getNote(@PathVariable("noteId") String noteId) {
-        return noteBo.getSelectedNote(noteId);
+        return noteService.getSelectedNote(noteId);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{noteId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public void updateNote(@PathVariable ("noteId") String noteId, @RequestBody NoteDTO noteDTO) {
-        boolean isUpdated = noteBo.updateNote(noteId, noteDTO);
+        noteService.updateNote(noteId, noteDTO);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping(value = "/{noteId}")
     public void deleteNote(@PathVariable ("noteId") String noteId) {
-        boolean isDeleted = noteBo.deleteNote(noteId);
+        noteService.deleteNote(noteId);
     }
 }
---
+++ src/main/java/lk/ijse/notetaker/entity/NoteEntity.java
@@ -0,0 +1,22 @@
+package lk.ijse.notetaker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "notes")
+@Entity
+public class NoteEntity {
+    @Id
+    private String id;
+    private String noteTitle;
+    private String noteDesc;
+    private String priorityLevel;
+    private String createDate;
+}
